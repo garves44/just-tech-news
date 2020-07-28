ac5cd34ad79bf0752f6e61b3af0dae4f0bf62314
Garves <garves44@gmail.com>
2020-07-28 13:07:43 -0600

    added comment routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRouts = require('./comment-routes')''
 
 router.use('/users', userRoutes);
 router.use('/users', postRoutes);
+router.use('/comments', commentRouts);
 
 module.exports = router;
